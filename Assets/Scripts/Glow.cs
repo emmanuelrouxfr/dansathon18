1829574d7e8b52ff5f742eb55873adbac056cb7b
roux <emmanuel.roux@creatis.insa-lyon.fr>
2018-09-29 15:21:25 +0200

    We changed the cube with the arrows and glowing points

---
+++ Assets/Scripts/Glow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glow : MonoBehaviour {
+
+    public float delay;
+    public float cycle;
+    [Range(0,1)]
+    public float onTime;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float cycleTime = (Time.time + delay) % cycle;
+
+        GetComponent<Renderer>().enabled = cycleTime < (onTime * cycle);
+	}
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour {
+
+    [Header("Prefabs")]
+    public GameObject flechePrefab;
+    public GameObject pointPrefab;
+
+    public Glow fleche;
+    public Glow[] points;
+
+    [Header("Setup")]
+    public int numPoints;
+    public float cycleTime;
+    [Range(0, 1)]
+    public float onTime;
+
+	// Use this for initialization
+	void Start () {
+        fleche = Instantiate(flechePrefab).GetComponent<Glow>();
+        fleche.transform.parent = transform;
+        points = new Glow[numPoints];
+        for(int i=0;i<numPoints;i++)
+        {
+            points[i] = Instantiate(pointPrefab).GetComponent<Glow>();
+            points[i].transform.localPosition = new Vector3(i * 1f, 0f, 0f);
+            points[i].transform.parent = transform;
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        fleche.transform.parent = transform;
+        for (int i = 0; i < numPoints; i++)
+        {
+            points[i].delay = (i * 1f / numPoints) * cycleTime;
+            points[i].cycle = cycleTime;
+            points[i].onTime = onTime;
+
+            points[i].transform.localPosition = new Vector3(i * 1f, 0f, 0f);
+            points[i].transform.parent = transform;
+        }
+
+        fleche.delay = 1;
+        fleche.cycle = cycleTime;
+        fleche.onTime = onTime;
+
+    }
+
+        Vector3 getPosInPath(float progress)
+    {
+        return new Vector3(); // to fill
+    }
+}
